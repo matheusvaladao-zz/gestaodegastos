8b543d3f8dd581660777af2607231334c5a0e5b8
Matheus <matheusvaladao5@gmail.com>
2020-07-28 19:53:35 -0300

    Criando projeto relativo a API que receberá as requisições

---
+++ gestaodegastos-api/src/main/java/com/gestaodegastos/gestaodegastosapi/GestaodegastosApiApplication.java
@@ -0,0 +1,13 @@
+package com.gestaodegastos.gestaodegastosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodegastosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodegastosApiApplication.class, args);
+	}
+
+}
---
+++ gestaodegastos-api/src/test/java/com/gestaodegastos/gestaodegastosapi/GestaodegastosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaodegastos.gestaodegastosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodegastosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
